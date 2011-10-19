732873e355e5810f3f697f21aff7ba6e86af7700
jack <jack.ha@nelen-schuurmans.nl>
2011-10-19 10:55:19 +0200

    Updated geoobject import for new files

--- lizard_area/geoobject.py
+++ lizard_area/geoobject.py
@@ -47,6 +47,8 @@ def import_shapefile_area(shapefile_filename, user, data_administrator):
         created_by=user)
     geo_object_group.save()
 
+    done = {}  # Check for duplicates
+
     # Add all geo objects to group.
     for feature in layer:
         geom = feature.GetGeometryRef()
@@ -55,38 +57,71 @@ def import_shapefile_area(shapefile_filename, user, data_administrator):
         # geom.FlattenTo2D()
         # import pdb;pdb.set_trace()
 
-        # KRW Waterlichamen merge
-        kwargs = {
-            'ident': feature.GetField(feature.GetFieldIndex('OWAIDENT')),
-            'geometry': GEOSGeometry(geom.ExportToWkt(), srid=4326),
-            'geo_object_group': geo_object_group,
+        # # KRW Waterlichamen merge
+        # kwargs = {
+        #     'ident': feature.GetField(feature.GetFieldIndex('OWAIDENT')),
+        #     'geometry': GEOSGeometry(geom.ExportToWkt(), srid=4326),
+        #     'geo_object_group': geo_object_group,
 
-            # Communique
-            'name': '%s' % feature.GetField(feature.GetFieldIndex('OWANAAM')),
+        #     # Communique
+        #     'name': '%s' % feature.GetField(feature.GetFieldIndex('OWANAAM')),
 
-            # Area
-            'data_administrator': data_administrator,
-            'area_class': Area.AREA_CLASS_KRW_WATERLICHAAM,
-        }
+        #     # Area
+        #     'data_administrator': data_administrator,
+        #     'area_class': Area.AREA_CLASS_KRW_WATERLICHAAM,
+        # }
 
-        # area_code, _ = AreaCode.objects.get_or_create(
-        #     name=feature.GetField(feature.GetFieldIndex('GAFCODE')))
-        # # Afvoergebieden
+        # # Afvoergebieden, 2 varianten
+        # # area_code, _ = AreaCode.objects.get_or_create(
+        # #     name=feature.GetField(feature.GetFieldIndex('GAFCODE')))
         # kwargs = {
         #     'ident': feature.GetField(feature.GetFieldIndex('GAFIDENT')),
         #     'geometry': GEOSGeometry(geom.ExportToWkt(), srid=4326),
         #     'geo_object_group': geo_object_group,
 
         #     # Communique
-        #     'name': '%s' % feature.GetField(feature.GetFieldIndex('GAFNAAM')),
-        #     'code': area_code,
+        #     'name': unicode(
+        #         feature.GetField(feature.GetFieldIndex('GAFNAAM')),
+        #         errors='ignore'),
+        #     #'code': area_code,
         #     # Area
         #     'data_administrator': data_administrator,
         #     'area_class': Area.AREA_CLASS_AAN_AFVOERGEBIED,
         # }
 
-        geo_object = Area(**kwargs)
-        geo_object.save()
+        # Deel afvoergebieden: watervlakken_mbp_sap
+        # area_code, _ = AreaCode.objects.get_or_create(
+        #     name=feature.GetField(feature.GetFieldIndex('GAFCODE')))
+        parent = Area.objects.get(name=unicode(
+                feature.GetField(feature.GetFieldIndex('GAFNAAM')),
+                errors='ignore'))
+        # Only field that is unique.
+        name = 'Deelgebied %s' % feature.GetField(
+            feature.GetFieldIndex('FID_GBKNwa'))
+        ident = 'LSDR-%s' % feature.GetField(
+            feature.GetFieldIndex('FID_GBKNwa')),  # Else it is not unique
+        kwargs = {
+            'ident': ident,
+            'geometry': GEOSGeometry(geom.ExportToWkt(), srid=4326),
+            'geo_object_group': geo_object_group,
+
+            # Communique
+            'name': name,
+            #'code': area_code,
+            # Area
+            'data_administrator': data_administrator,
+            'area_class': Area.AREA_CLASS_DEEL_AAN_AFVOERGEBIED,
+            'parent': parent,
+        }
+
+        if ident not in done:
+            geo_object = Area(**kwargs)
+            geo_object.save()
+            done[ident] = None
+        else:
+            logger.warning(
+                'Ignored: Ident %s is already imported in this session' %
+                ident)
         number_of_features += 1
 
     logger.info("Added %s with %s geo objects.",
